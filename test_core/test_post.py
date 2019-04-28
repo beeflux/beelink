e13d2b1d0ebeae462ce6722db6770d51748825e8
awemulya <awemulya@gmail.com>
2019-04-28 12:26:28 +0545

    bettter test implementation

--- test_core/test_post.py
+++ test_core/test_post.py
@@ -3,14 +3,18 @@ from core.post import Post
 
 
 class TestPost(unittest.TestCase):
+    def setUp(self):
+        self.post = Post(1)
+
+    def tearDown(self):
+        self.post = None
+
     def test_post_type(self):
-        post = Post(1)
-        assert post.type_display == "Text"
+        assert self.post.type_display == "Text"
 
     def test_post_new(self):
-        post = Post(1)
-        post.post_text("hello my first post")
-        self.assertTrue(post.text != "")
+        self.post.post_text("hello my first post")
+        self.assertTrue(self.post.text != "")
 
 
 if __name__ == '__main__':
